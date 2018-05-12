7cf507eecb9231e7378e789da4bb48469187fd2d
ALEXSUPERCOMP\alexa <alexander12356@inbox.ru>
2018-05-12 15:12:22 +0900

    moving jump player

---
+++ Assets/JPT/Scripts/Gameplay/MovementClasses/MovementController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace JPT.Gameplay.MovementClasses
+{
+    public class MovementController : MonoBehaviour
+    {
+        private Rigidbody2D m_Rigidbody2D = null;
+
+        [SerializeField] private float m_Speed = 0f;
+        [SerializeField] private float m_JumpForce = 0f;
+
+        [Space]
+        [SerializeField] private Transform m_CheckGroundTransform = null;
+        [SerializeField] private float m_OverlapRadius = 0f;
+
+        private void Awake()
+        {
+            m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        public void MoveHorizontal(float value)
+        {
+            var velocity = new Vector2(value * m_Speed * Time.fixedDeltaTime, m_Rigidbody2D.velocity.y);
+
+            m_Rigidbody2D.velocity = velocity;
+        }
+
+        public void Jump()
+        {
+            var overlappedCollider = Physics2D.OverlapCircle(m_CheckGroundTransform.position, m_OverlapRadius, 1 << LayerMask.NameToLayer("Platform"));
+            if (!overlappedCollider)
+            {
+                Debug.Log("Cannot jump");
+                return;
+            }
+
+            m_Rigidbody2D.AddForce(Vector2.up * m_JumpForce);
+
+            Debug.Log("GAME_LOG: player jump");
+        }
+    }
+}
---
+++ Assets/JPT/Scripts/Gameplay/PlayerClasses/PlayerInputController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using UnityEngine;
+using UnityEngine.Events;
+
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace JPT.Gameplay.PlayerClasses
+{
+    public class PlayerInputController : MonoBehaviour
+    {
+        [Serializable]
+        public class HorizontalInputEvent : UnityEvent<float> { }
+
+        [SerializeField] private HorizontalInputEvent m_HorizontalInputEvent = null;
+        [SerializeField] private UnityEvent m_JumpInputEvent = null;
+
+        private void Update()
+        {
+            var axisValue = CrossPlatformInputManager.GetAxis("Horizontal");
+
+            m_HorizontalInputEvent?.Invoke(axisValue);
+
+            if (CrossPlatformInputManager.GetButtonDown("Jump"))
+            {
+                m_JumpInputEvent.Invoke();
+                Debug.Log("GAME_LOG: Pushed jump button");
+            }
+        }
+    }
+}
