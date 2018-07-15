5099ef1ec5674553d0d3d874af459c66cd203d82
ALEXSUPERCOMP\alexa <alexander12356@inbox.ru>
2018-07-15 15:41:07 +0900

    main menu now run player test

---
+++ Assets/JPT/Scripts/UI/SceneRunner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace JPT.UI
+{
+    public class SceneRunner : MonoBehaviour
+    {
+        public void SceneRun(string sceneId)
+        {
+            SceneManager.LoadScene(sceneId);
+        }
+    }
+}
